fe6814be450e4b6706f03a51432e4acfd88dfbf3
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-10-06 07:21:35 -0300

    feat: update title, deadline and done by id

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   const todoCreate = {
-    id: uuidv4(),
+    id: uuidv4(), 
     title,
     done: false,
     deadline: new Date(deadline),
@@ -62,11 +62,33 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  if(!todo){
+    return response.status(404).json({ error: "To Do not found!"});
+  }
+  
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(201).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  
+  const todo = user.todos.find((todo) => todo.id === id);
+  if(!todo){
+    return response.status(404).json({ error: "To Do not found!"});
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
